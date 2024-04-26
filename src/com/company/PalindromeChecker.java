d02d22d3a8733cc18c99ae6e8f2bb50cdacd1053
Avi Bhatnagar <91671078+Avibhatnagar10@users.noreply.github.com>
2024-04-27 01:09:37 +0530

    Add files via upload

---
+++ src/com/company/PalindromeChecker.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("The string \"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
