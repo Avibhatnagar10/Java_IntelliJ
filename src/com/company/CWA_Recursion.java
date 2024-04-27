117bea190a40bc84d39610e40a6f1cd54e0a5706
Avi Bhatnagar <91671078+Avibhatnagar10@users.noreply.github.com>
2024-04-27 16:25:52 +0530

    Add files via upload

---
+++ src/com/company/CWA_Recursion.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class CWA_Recursion {
+    static int  factorial(int n){
+        if(n==0 || n==1){
+            return 1;
+        }else{
+            return  n*factorial(n-1);
+        }
+    }
+
+    public static void main(String[] args) {
+int num =5;
+int result =factorial(num);
+        System.out.println("Factorial of " +num +" is: "+result );
+    }
+}
