2cc29f5908da450414d05fbad912df1e013d64a7
Avi Bhatnagar <91671078+Avibhatnagar10@users.noreply.github.com>
2024-04-28 23:29:48 +0530

    Add files via upload

---
+++ src/com/company/CWA_PractiseSet_Methods.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class CWA_PractiseSet_Methods {
+    static void multiplication(int n)
+    {
+        for(int i=1;i<=10;i++){
+            System.out.format("%d X %d = %d\n", n , i , n*i);
+        }
+    }
+
+    public static void main(String[] args) {
+        multiplication(7);
+    }
+}
